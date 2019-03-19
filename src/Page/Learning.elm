a7733526dea5e4acc1657ca5a6c273d9e6edcf96
Dominique Feyer <dfeyer@ttree.ch>
2019-03-19 16:48:29 +0100

    TASK: Implement breadcrum in Learnings

--- src/Page/Learning.elm
+++ src/Page/Learning.elm
@@ -6,7 +6,9 @@ import Data.Section as Section
 import Data.Slugable exposing (collectionView, compactCollectionView)
 import Data.Target as Target
 import Data.Topic as Topic
-import Html exposing (Html, div, span, text)
+import Html exposing (Html, a, div, span, text)
+import Html.Attributes exposing (class, href)
+import Html.Events exposing (onClick)
 import Session exposing (Session)
 import Shared exposing (Group, Section, Slugable, SlugableTarget, Target, Topic)
 import Views.Layout exposing (mainHeaderView, mainHeaderWithChapterView, rowView)
@@ -65,7 +67,9 @@ view model =
                         ( groupPosition, Nothing ) ->
                             case groupPosition of
                                 ( group, Nothing ) ->
-                                    [ mainHeaderWithChapterView (text "Apprentissages") (Group.toHtml group)
+                                    [ mainHeaderWithChapterView
+                                        groupBreadcrumb
+                                        (text (Group.toString group))
                                     , viewPosition
                                         (Learnings.topicByGroup model.learnings group)
                                         (goToTopic group)
@@ -73,7 +77,9 @@ view model =
                                     ]
 
                                 ( group, Just topic ) ->
-                                    [ mainHeaderWithChapterView (text "Apprentissages") (Group.toHtml group)
+                                    [ mainHeaderWithChapterView
+                                        (topicBreadcrumb group)
+                                        (text (Topic.toString topic))
                                     , viewPositionCompact
                                         (Learnings.sectionByTopic model.learnings group topic)
                                         (goToSection group topic)
@@ -83,7 +89,9 @@ view model =
                         ( ( group, Just topic ), Just sectionPosition ) ->
                             case sectionPosition of
                                 ( section, Nothing ) ->
-                                    [ mainHeaderWithChapterView (text "Apprentissages") (Group.toHtml group)
+                                    [ mainHeaderWithChapterView
+                                        (sectionBreadcrumb group topic)
+                                        (text (Section.toString section))
                                     , viewPositionCompact
                                         (Learnings.targetBySection model.learnings group topic section)
                                         (goToTarget group topic section)
@@ -91,12 +99,55 @@ view model =
                                     ]
 
                                 ( section, Just target ) ->
-                                    [ div [] [ text "TODO Target detail" ] ]
+                                    [ mainHeaderWithChapterView
+                                        (sectionBreadcrumb group topic)
+                                        (text (Section.toString section))
+                                    , div [ class "teaser" ] [ text (Target.toString target) ]
+                                    ]
 
                         _ ->
                             [ viewError ]
             )
     }
+
+
+groupBreadcrumb : Html Msg
+groupBreadcrumb =
+    breadcrumb
+        [ ( Nothing, "Apprentissages" )
+        ]
+
+
+topicBreadcrumb : Group -> Html Msg
+topicBreadcrumb group =
+    breadcrumb
+        [ ( Nothing, "Apprentissages" )
+        , ( Just ( ( group, Nothing ), Nothing ), Group.toString group )
+        ]
+
+
+sectionBreadcrumb : Group -> Topic -> Html Msg
+sectionBreadcrumb group topic =
+    breadcrumb
+        [ ( Nothing, "Apprentissages" )
+        , ( Just ( ( group, Nothing ), Nothing ), Group.toString group )
+        , ( Just ( ( group, Just topic ), Nothing ), Topic.toString topic )
+        ]
+
+
+breadcrumb : List ( Maybe Position, String ) -> Html Msg
+breadcrumb items =
+    items
+        |> List.map
+            (\( p, l ) ->
+                case p of
+                    Just p_ ->
+                        a [ href "#", onClick (GotPosition p_) ] [ text l ]
+
+                    Nothing ->
+                        a [ href "#", onClick GotRoot ] [ text l ]
+            )
+        |> span [ class "breadcrumb" ]
 
 
 viewPosition : Maybe (List (Slugable a)) -> (Slugable a -> msg) -> (Slugable a -> Html msg) -> Html msg
@@ -130,6 +181,7 @@ viewError =
 
 type Msg
     = GotSession Session
+    | GotRoot
     | GotPosition Position
 
 
@@ -158,6 +210,9 @@ update msg model =
     case msg of
         GotSession session ->
             ( { model | session = session }, Cmd.none )
+
+        GotRoot ->
+            ( { model | position = Nothing }, Cmd.none )
 
         GotPosition position ->
             ( { model | position = Just position }, Cmd.none )
