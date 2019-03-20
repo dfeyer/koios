3b4e4db969da935f21298c083de6da85be58f1e5
Dominique Feyer <dfeyer@ttree.ch>
2019-03-20 14:41:46 +0100

    TASK: Better login page

--- src/Page/Login.elm
+++ src/Page/Login.elm
@@ -1,8 +1,8 @@
 module Page.Login exposing (Model, Msg, init, subscriptions, toSession, update, view)
 
-import Html exposing (Html, a, div, form, input, label, span, text)
-import Html.Attributes exposing (class, href, id, name, placeholder, type_, value)
-import Html.Events exposing (onClick)
+import Html exposing (Html, a, button, div, form, input, label, span, text)
+import Html.Attributes exposing (class, href, id, name, placeholder, required, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 import Session exposing (Session)
 import Views.Layout exposing (mainHeaderView)
 
@@ -14,18 +14,24 @@ import Views.Layout exposing (mainHeaderView)
 type alias Model =
     { session : Session
     , connectionType : ConnectionType
+    , username : Maybe String
+    , email : Maybe String
+    , phone : Maybe String
     }
 
 
 type ConnectionType
-    = ShortMessage
+    = Email
     | Phone
 
 
 init : Session -> ( Model, Cmd Msg )
 init session =
     ( { session = session
-      , connectionType = ShortMessage
+      , connectionType = Email
+      , username = Nothing
+      , email = Nothing
+      , phone = Nothing
       }
     , Cmd.none
     )
@@ -40,62 +46,71 @@ view model =
     { title = "Connexion | Mon carnet de board IHES"
     , content =
         div []
-            [ mainHeaderView (text "Connexion")
-            , div [ class "login-form" ]
-                [ form [ class "login-form__form" ]
-                    [ label [ class "form__label" ]
-                        [ text "Utilisateur"
-                        , input
-                            [ class "form__input"
-                            , type_ "text"
-                            , id "username"
-                            , name "username"
-                            ]
-                            []
-                        ]
-                    , label [ class "form__label" ]
-                        (case model.connectionType of
-                            Phone ->
-                                [ text "Numéro de téléphone"
-                                , input
-                                    [ class "form__input"
-                                    , type_ "text"
-                                    , id "username"
-                                    , name "phone_number"
-                                    , placeholder "+41 78 111 11 11"
-                                    ]
-                                    []
-                                ]
-
-                            ShortMessage ->
-                                [ text "Courrier électronique"
-                                , input
-                                    [ class "form__input"
-                                    , type_ "text"
-                                    , id "email"
-                                    , name "email"
-                                    , placeholder "vous@fournisseur.com"
-                                    ]
-                                    []
-                                ]
-                        )
-                    , div [ class "form__actions" ]
-                        [ input [ class "form__button", type_ "submit", value "Se connecter" ] []
-                        ]
-                    , div [ class "form__subactions" ]
-                        (case model.connectionType of
-                            Phone ->
-                                [ a [ class "form__subaction", href "#", onClick (ConnectionTypeSwitched ShortMessage) ] [ text "Connexion par courrier électronique ?" ]
-                                ]
-
-                            ShortMessage ->
-                                [ a [ class "form__subaction", href "#", onClick (ConnectionTypeSwitched Phone) ] [ text "Connexion par SMS ?" ]
-                                ]
-                        )
-                    ]
+            [ div [ class "login-form" ]
+                [ loginFormView model
                 ]
             ]
     }
+
+
+loginFormView : Model -> Html Msg
+loginFormView model =
+    form
+        [ class "login-form__form"
+        , onSubmit LoginRequested
+        ]
+        [ mainHeaderView (text "Connexion")
+        , label [ class "form__label" ]
+            (case model.connectionType of
+                Phone ->
+                    [ text "Numéro de téléphone"
+                    , input
+                        [ class "form__input"
+                        , type_ "text"
+                        , id "username"
+                        , name "phone_number"
+                        , placeholder "+41 78 111 11 11"
+                        , required True
+                        , onInput PhoneEdited
+                        ]
+                        []
+                    , div [ class "form__help" ]
+                        [ text "Si votre compte n'existe pas ce numéro de téléphone deviendra votre identifiant" ]
+                    ]
+
+                Email ->
+                    [ text "Courrier électronique"
+                    , input
+                        [ class "form__input"
+                        , type_ "text"
+                        , id "email"
+                        , name "email"
+                        , placeholder "vous@fournisseur.com"
+                        , required True
+                        , onInput EmailEdited
+                        ]
+                        []
+                    , div [ class "form__help" ]
+                        [ text "Si votre compte n'existe pas cette adresse de courrier électronique deviendra votre identifiant" ]
+                    ]
+            )
+        , div [ class "form__actions" ]
+            [ button
+                [ class "form__button"
+                ]
+                [ text "Se connecter" ]
+            ]
+        , div [ class "form__subactions" ]
+            (case model.connectionType of
+                Phone ->
+                    [ a [ class "form__subaction", href "#", onClick (ConnectionTypeSwitched Email) ] [ text "Connexion par courrier électronique ?" ]
+                    ]
+
+                Email ->
+                    [ a [ class "form__subaction", href "#", onClick (ConnectionTypeSwitched Phone) ] [ text "Connexion par SMS ?" ]
+                    ]
+            )
+        ]
 
 
 
@@ -105,6 +120,10 @@ view model =
 type Msg
     = GotSession Session
     | ConnectionTypeSwitched ConnectionType
+    | UsernameEdited String
+    | EmailEdited String
+    | PhoneEdited String
+    | LoginRequested
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -115,6 +134,18 @@ update msg model =
 
         ConnectionTypeSwitched connectionType ->
             ( { model | connectionType = connectionType }, Cmd.none )
+
+        UsernameEdited username ->
+            ( { model | username = Just username }, Cmd.none )
+
+        EmailEdited email ->
+            ( { model | email = Just email }, Cmd.none )
+
+        PhoneEdited phone ->
+            ( { model | phone = Just phone }, Cmd.none )
+
+        LoginRequested ->
+            ( model, Cmd.none )
 
 
 
